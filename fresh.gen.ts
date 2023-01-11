042297a572ebc94e199ebdfe19be1732bc80c1e8
Kaisei <me@kaisei.dev>
2023-01-11 20:17:37 +0900

    change: npm to esm

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -4,17 +4,15 @@
 
 import config from "./deno.json" assert { type: "json" };
 import * as $0 from "./routes/[slug].tsx";
-import * as $1 from "./routes/api/joke.ts";
-import * as $2 from "./routes/gfm.css.ts";
-import * as $3 from "./routes/index.tsx";
+import * as $1 from "./routes/gfm.css.ts";
+import * as $2 from "./routes/index.tsx";
 import * as $$0 from "./islands/Counter.tsx";
 
 const manifest = {
   routes: {
     "./routes/[slug].tsx": $0,
-    "./routes/api/joke.ts": $1,
-    "./routes/gfm.css.ts": $2,
-    "./routes/index.tsx": $3,
+    "./routes/gfm.css.ts": $1,
+    "./routes/index.tsx": $2,
   },
   islands: {
     "./islands/Counter.tsx": $$0,
