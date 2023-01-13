c8d8f8271c2286699d813b9891dca74a878fd8af
Kaisei <me@kaisei.dev>
2023-01-13 15:25:51 +0900

    change: color

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,9 +1,9 @@
 export default function Header() {
   return (
-    <div class="bg-blue-300">
+    <div class="bg-black">
       <header class="mx-auto max-w-screen-lg flex gap-3 justify-between"> 
         <div class="p-4 flex">
-          <h1 class="text-xl ">
+          <h1 class="text-xl font-bold text-white">
             Documents
           </h1>
         </div>
